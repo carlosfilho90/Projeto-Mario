472e2e4426e9ef7b33978cbbc187fcec735c5d2c
carlosfilho90 <108691508+carlosfilho90@users.noreply.github.com>
2023-01-26 14:40:28 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const botaoTrailer = document.querySelector('.butao-trailer');
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo)
+})
+
+botaoFecharModal.addEventListener("click", () =>{
+    alternarModal();
+ video.setAttribute("src", "");
+})
+
